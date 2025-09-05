24603fb34a1d916873a1093dce13dd5ded45cc81
gabrielle-carvalho <gabiecarvalho15@gmail.com>
2025-09-04 21:14:50 -0300

    feat: add file transfer functionality to remetenteArqTCP and receptorArqTCP

--- aula01/arquivos/receptorArqTCP.java
+++ aula01/arquivos/receptorArqTCP.java
@@ -1,16 +1,42 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
-
 
 public class receptorArqTCP {
     public static void main(String[] args) {
-        public static void main (String[] args){
-            try {
-                Socket cliente = new Socket("localhost", 12345);
-                ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-                
-            } catch (Exception e) {
+        if (args.length != 1) {
+            System.err.println("ReptorArqTCP <porta>");
+            System.exit(1);
+        }
+        try {
+            int porta = Integer.parseInt(args[0]);
+            try (ServerSocket serverSocket = new ServerSocket(porta)) {
+                System.out.println("aguardando conexão na porta " + porta);
+
+                Socket socket = serverSocket.accept();
+                System.out.println("conectado");
+
+                InputStream input = socket.getInputStream();
+                FileOutputStream fileOut = new FileOutputStream("recebido.txt");
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = input.read(buffer)) != -1) {
+                    fileOut.write(buffer, 0, bytesRead);
+                }
+
+                fileOut.close();
+                socket.close();
+                System.out.println("recebido");
             }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 }
+
--- aula01/arquivos/remetenteArqTCP.java
+++ aula01/arquivos/remetenteArqTCP.java
@@ -1,5 +1,37 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class remetenteArqTCP {
     public static void main(String[] args) {
-        
+        if (args.length != 3) {
+            System.err.println("coloque <servidor> <porta> <arquivo>");
+            System.exit(1);
+        }
+
+        String servidor = args[0];
+        int porta = Integer.parseInt(args[1]);
+        String caminho = args[2];
+
+        try (Socket socket = new Socket(servidor, porta)) {
+            File file = new File(caminho);
+            FileInputStream fileIn = new FileInputStream(file);
+
+            OutputStream output = socket.getOutputStream();
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = fileIn.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+
+            fileIn.close();
+            socket.close();
+            System.out.println("enviado");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
